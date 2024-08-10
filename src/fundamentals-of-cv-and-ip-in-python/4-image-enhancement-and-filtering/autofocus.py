5e5dd597e06c65912e31de2bfb3cb033ad0614a3
Tymur Lysenko <19238280+Sitiritis@users.noreply.github.com>
2024-08-10 14:30:08 -0400

    Finish autofocus

--- src/fundamentals-of-cv-and-ip-in-python/4-image-enhancement-and-filtering/autofocus.py
+++ src/fundamentals-of-cv-and-ip-in-python/4-image-enhancement-and-filtering/autofocus.py
@@ -25,13 +25,16 @@
 # After calculating **focus measure** for each image in the series, the one with the highest number is an image with the most details. This is the output of the algorithm - the image with the desired object in focus.
 
 # %%
-import cv2
-import numpy as np
-from numpy.typing import NDArray
-import matplotlib.pyplot as plt
-from typing import Literal, Final
+from dataclasses import dataclass
+from functools import total_ordering
 from pathlib import Path
+from typing import Final, Literal, Self
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
 from dataPath import DATA_PATH
+from numpy.typing import NDArray
 
 # %%
 source_video_path = Path(DATA_PATH) / "videos" / "focus-test.mp4"
@@ -75,6 +78,7 @@ focus_measure_expected_input_dimensions: Final[Literal[2]] = 2
 #
 # Focus measures based on Laplacian rely on the idea that for a sharpaer image the 2-nd derivatives of the image will have more higher values than for a blurry image. See the examples of appyling Laplacian to a blurry and sharp images below.
 
+
 # %%
 def normalized_laplacian(bgr_image: NDArray[np.generic]) -> NDArray[np.float64]:
   """Given a BGR image calculates a normalized laplacian of the image.
@@ -82,14 +86,7 @@ def normalized_laplacian(bgr_image: NDArray[np.generic]) -> NDArray[np.float64]:
   The output can be displayed as a grayscale image.
   """
   grayscaled = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
-  laplacian = cv2.Laplacian(
-    grayscaled,
-    ddepth=cv2.CV_64F,
-    ksize=3,
-    scale=1,
-    delta=0,
-    borderType=cv2.BORDER_REFLECT_101
-  )
+  laplacian = cv2.Laplacian(grayscaled, ddepth=cv2.CV_64F, ksize=3, scale=1, delta=0, borderType=cv2.BORDER_REFLECT_101)
   return cv2.normalize(
     laplacian,
     dst=laplacian,
@@ -97,7 +94,7 @@ def normalized_laplacian(bgr_image: NDArray[np.generic]) -> NDArray[np.float64]:
     beta=0,
     norm_type=cv2.NORM_MINMAX,
     dtype=cv2.CV_64F,
-  )
+  )  # type: ignore[return-value]
 
 
 # %%
@@ -160,11 +157,12 @@ plt.imshow(clear_image_laplacian_normalized, cmap="gray")
 #
 # The focus measure is defined in [Diatom autofocusing in brightheld microscopy: a comparative study](https://decsai.ugr.es/vip/files/conferences/Autofocusing2000.pdf).
 
+
 # %%
 def var_abs_laplacian(
   grayscale_image: NDArray[np.generic],
-  ksize: int=3,
-  border_type: int=cv2.BORDER_REFLECT_101,
+  ksize: int = 3,
+  border_type: int = cv2.BORDER_REFLECT_101,
 ) -> float:
   """Calculate variance of absolute values of Laplacian for a given **grayscale** `image`.
 
@@ -174,14 +172,16 @@ def var_abs_laplacian(
     error_message = f"The input image must be a grayscale image, however, the dimensions are {grayscale_image.shape}"
     raise Exception(error_message)
 
-  abs_laplacian = abs(cv2.Laplacian(
-    grayscale_image,
-    ddepth=cv2.CV_64F,
-    ksize=ksize,
-    scale=1,
-    delta=0,
-    borderType=border_type,
-  ))
+  abs_laplacian = abs(
+    cv2.Laplacian(
+      grayscale_image,
+      ddepth=cv2.CV_64F,
+      ksize=ksize,
+      scale=1,
+      delta=0,
+      borderType=border_type,
+    )
+  )
   # Don't calcualte the variance using the standard numy method.
   # Since the algorithm just looks for a bigger number, it is okay to not divide the result by the number of pixels
   mean_abs_laplacian = abs_laplacian.mean()
@@ -193,12 +193,13 @@ def var_abs_laplacian(
 #
 # The focus measure is defined in section 5 in [Shape from Focus](http://www1.cs.columbia.edu/CAVE/publications/pdfs/Nayar_TR89.pdf).
 
+
 # %%
 def sum_modified_laplacian(
   grayscale_image: NDArray[np.generic],
-  ksize: int=3,
-  threshold: int=0,
-  border_type: int=cv2.BORDER_REFLECT_101,
+  ksize: int = 3,
+  threshold: int = 0,
+  border_type: int = cv2.BORDER_REFLECT_101,
 ) -> float:
   """Calculate Sum Modified Laplacian (SML) for a given **grayscale** `image`.
 
@@ -222,20 +223,24 @@ def sum_modified_laplacian(
     ktype=cv2.CV_32F,
   )
   # Calculate the 2-nd order derivatives of the image in x and y directoin individually and take their absolute value.
-  dx2_abs = abs(cv2.sepFilter2D(
-    grayscale_image,
-    ddepth=cv2.CV_32F,
-    kernelX=kdxx,
-    kernelY=kdxy,
-    borderType=border_type,
-  ))
-  dy2_abs = abs(cv2.sepFilter2D(
-    grayscale_image,
-    ddepth=cv2.CV_32F,
-    kernelX=kdyx,
-    kernelY=kdyy,
-    borderType=border_type,
-  ))
+  dx2_abs = abs(
+    cv2.sepFilter2D(
+      grayscale_image,
+      ddepth=cv2.CV_32F,
+      kernelX=kdxx,
+      kernelY=kdxy,
+      borderType=border_type,
+    )
+  )
+  dy2_abs = abs(
+    cv2.sepFilter2D(
+      grayscale_image,
+      ddepth=cv2.CV_32F,
+      kernelX=kdyx,
+      kernelY=kdyy,
+      borderType=border_type,
+    )
+  )
   # The Modified Laplacian is a sum of the absolute values of the 2-nd order partial derivatives.
   modified_laplacian = dx2_abs + dy2_abs
   # Sum the Modified Laplacians that are greater than the threshold
@@ -253,35 +258,147 @@ def sum_modified_laplacian(
 
 # %%
 cap = cv2.VideoCapture(str(source_video_path))
-_, frame = cap.read()
-cap.release()
 
 print(f"Frame resolution: {cap.get(cv2.CAP_PROP_FRAME_WIDTH)}x{cap.get(cv2.CAP_PROP_FRAME_HEIGHT)}")
 
-top = 450
-left = 1300
-bottom = 1600
-right = 2400
+_, frame = cap.read()
 
-frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-frame = cv2.rectangle(
-  frame,
-  pt1=(left, top),
-  pt2=(right, bottom),
-  color=(255, 0, 0),
-  thickness=3,
-  lineType=cv2.LINE_AA
+cap.release()
+
+# Resize the resolution to HD, so that the algorithm will run more efficiently
+resized_frame = cv2.resize(frame, dsize=(1280, 720), interpolation=cv2.INTER_AREA)
+
+# Original dimensions of the object
+# top = 450
+# left = 1300
+# bottom = 1600
+# right = 2400
+top = 150
+left = 430
+bottom = 550
+right = 800
+
+# frame = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2RGB)
+resized_frame_selected_object = cv2.rectangle(
+  resized_frame, pt1=(left, top), pt2=(right, bottom), color=(0, 0, 255), thickness=3, lineType=cv2.LINE_AA
 )
 
 plt.figure(figsize=(10, 10))
-plt.imshow(frame)
+plt.imshow(resized_frame_selected_object[..., ::-1])
+
 
 # %% [markdown]
 # #### Apply the focus measures
 
+
+# %%
+@dataclass(frozen=True)
+# Note, even though this decorator is called total_ordering, this class is actually not a total order
+@total_ordering
+class FocusMeasure:
+  frame_number: int
+  """Non-negative number"""
+  focus_measure: float
+
+  def __le__(self: Self, other: "FocusMeasure") -> bool:
+    """The defintion of < implies that this class is not a total order.
+
+    E.g. if 2 frames have the same focus measures, but different frame_numbers, they will not be equal.
+    But > and < coparison will return False.
+    """
+    return self.focus_measure < other.focus_measure
+
+
 # %%
 cap = cv2.VideoCapture(str(source_video_path))
 
-# TODO
+try:
+  total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-cap.release()
+  most_focused_frame_var_abs_laplacian = FocusMeasure(
+    frame_number=-1,
+    focus_measure=0,
+  )
+  most_focused_frame_sum_modified_laplacian = FocusMeasure(
+    frame_number=-1,
+    focus_measure=0,
+  )
+
+  # Declare arrays to store focus measure for each frame to build a plot
+  var_abs_laplacians = np.zeros(total_frames)
+  sum_modified_laplacians = np.zeros(total_frames)
+
+  frame_grabbed, frame = cap.read()
+  while frame_grabbed:
+    next_frame_number = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
+    current_frame_number = next_frame_number - 1
+
+    resized_frame = cv2.resize(frame, dsize=(1280, 720), interpolation=cv2.INTER_AREA)
+    cropped_resized_frame = resized_frame[top:bottom, left:right, ...]
+    grayscaled_cropped_resized_frame = cv2.cvtColor(cropped_resized_frame, cv2.COLOR_BGR2GRAY)
+
+    current_frame_var_abs_laplacian = FocusMeasure(
+      frame_number=current_frame_number,
+      focus_measure=var_abs_laplacian(grayscaled_cropped_resized_frame),
+    )
+    current_frame_sum_modified_laplacian = FocusMeasure(
+      frame_number=current_frame_number,
+      focus_measure=sum_modified_laplacian(grayscaled_cropped_resized_frame),
+    )
+
+    var_abs_laplacians[current_frame_number] = current_frame_var_abs_laplacian.focus_measure
+    sum_modified_laplacians[current_frame_number] = current_frame_sum_modified_laplacian.focus_measure
+
+    if current_frame_var_abs_laplacian > most_focused_frame_var_abs_laplacian:
+      most_focused_frame_var_abs_laplacian = current_frame_var_abs_laplacian
+
+    if current_frame_sum_modified_laplacian > most_focused_frame_sum_modified_laplacian:
+      most_focused_frame_sum_modified_laplacian = current_frame_sum_modified_laplacian
+
+    frame_grabbed, frame = cap.read()
+finally:
+  cap.release()
+
+# %% [markdown]
+# Plot how the focus measure changes per frames
+
+# %%
+plt.figure(figsize=(18, 12))
+
+frame_numbers = np.arange(0, total_frames)
+
+plt.subplot(2, 1, 1)
+plt.title("Change of Variance of absolute values of Laplacian")
+plt.xlabel("Frame #")
+plt.ylabel("Variance of absolute values of Laplacian")
+plt.plot(frame_numbers, var_abs_laplacians, color="blue")
+
+plt.subplot(2, 1, 2)
+plt.title("Change of Sum Modified Laplacian (SML)")
+plt.xlabel("Frame #")
+plt.ylabel("Sum Modified Laplacian (SML)")
+plt.plot(frame_numbers, sum_modified_laplacians, color="orange")
+
+# %%
+cap = cv2.VideoCapture(str(source_video_path))
+
+try:
+  var_abs_laplacian_frame_no = most_focused_frame_var_abs_laplacian.frame_number
+  cap.set(cv2.CAP_PROP_POS_FRAMES, var_abs_laplacian_frame_no)
+  _, autofocused_frame_var_abs_laplacian = cap.read()
+
+  sml_frame_no = most_focused_frame_sum_modified_laplacian.frame_number
+  cap.set(cv2.CAP_PROP_POS_FRAMES, sml_frame_no)
+  _, autofocused_frame_sum_absolute_laplacian = cap.read()
+finally:
+  cap.release()
+
+plt.figure(figsize=(18, 12))
+
+plt.subplot(2, 1, 1)
+plt.title(f"Most focused frame as per variance of absolute values of Laplacian | frame #{var_abs_laplacian_frame_no}")
+plt.imshow(autofocused_frame_var_abs_laplacian[..., ::-1])
+
+plt.subplot(2, 1, 2)
+plt.title(f"Most focused frame as per Sum Modified Laplacian (SML) | frame #{sml_frame_no}")
+plt.imshow(autofocused_frame_sum_absolute_laplacian[..., ::-1])
